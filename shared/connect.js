8f1aa5c4ad97f1986eab95d3ea3fbedb3498ca56
manikanta1341 <medicharlamanikanta13@gmail.com>
2022-06-10 18:10:10 +0530

    first commit

--- shared/connect.js
+++ shared/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 module.exports.connect = async (req, res, next) => {
     try{
         let connection = await mongoose.connect(process.env.mongodb_url)
-        // console.log("connected")
+        
     }
     catch(err){
         res.send(err)
